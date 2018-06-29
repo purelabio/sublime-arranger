de54f3af5f1a335c901627c46173319923b13367
Yury Egorenkov <yury.egorenkov@gmail.com>
2018-06-29 16:01:53 +0200

    fix tabs

--- main.py
+++ main.py
@@ -8,15 +8,22 @@ class CursorVerticalAlignCommand(sublime_plugin.TextCommand):
     sel  = self.view.sel()
 
     row_first, col_first = view.rowcol(sel[0].a)
-
+    text_first = view.substr(view.line(sel[0].a))
+    tabs_count_first = text_first.count('\t')
     regions = []
     for cursor in sel:
       row, col = view.rowcol(cursor.a)
+
+      text  = view.substr(view.line(cursor.a))
+      tabs_count = text.count('\t')
+
+      tabs_diff = tabs_count_first - tabs_count
+
       point = view.text_point(row, col_first)
       regions.append(point)
-      text = view.substr(view.line(cursor.a))
+
       if col < col_first and len(text) < col_first:
-        self.view.insert(edit, cursor.a, " " * (col_first - col))
+        self.view.insert(edit, cursor.a, " " * (col_first - col + tabs_diff))
 
     sel.clear()
     sel.add_all(regions)
