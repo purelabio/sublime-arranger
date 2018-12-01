37d457286ab2b9d0fec60b479228d778e9c705fd
Yury Egorenkov <yury.egorenkov@gmail.com>
2018-12-01 11:24:22 +0100

    align by cmd+e and select region

--- main.py
+++ main.py
@@ -4,27 +4,28 @@ import sublime_plugin
 class CursorVerticalAlignCommand(sublime_plugin.TextCommand):
 
   def run(self, edit):
-    view = self.view
-    sel  = self.view.sel()
+    view                = self.view
+    sel                 = self.view.sel()
+    _, first_column     = view.rowcol(sel[0].a)
+    first_text          = view.substr(view.line(sel[0].a))
+    tabs_count_at_first = first_text.count('\t')
+    regions             = []
 
-    row_first, col_first = view.rowcol(sel[0].a)
-    text_first = view.substr(view.line(sel[0].a))
-    tabs_count_first = text_first.count('\t')
-    regions = []
     for cursor in sel:
-      row, col = view.rowcol(cursor.a)
+      row, col    = view.rowcol(cursor.a)
+      text        = view.substr(view.line(cursor.a))
+      tabs_count  = text.count('\t')
+      tabs_diff   = tabs_count_at_first - tabs_count
+      text_length = len(text)
+      point       = view.text_point(row, first_column)
 
-      text  = view.substr(view.line(cursor.a))
-      tabs_count = text.count('\t')
-
-      tabs_diff = tabs_count_first - tabs_count
-
-      point = view.text_point(row, col_first)
       regions.append(point)
 
-      text_len = len(text)
-      if col < col_first and text_len < col_first:
-        self.view.insert(edit, view.text_point(row, text_len), " " * (col_first - text_len + tabs_diff))
+      if col < first_column and text_length < first_column:
+        spaces     = " " * (first_column - text_length + tabs_diff)
+        text_point = view.text_point(row, text_length)
+
+        self.view.insert(edit, text_point, spaces)
 
     sel.clear()
     sel.add_all(regions)
@@ -33,12 +34,63 @@ class CursorVerticalAlignCommand(sublime_plugin.TextCommand):
 class TextVerticalAlignCommand(sublime_plugin.TextCommand):
 
   def run(self, edit):
-    view = self.view
-    sel  = self.view.sel()
-
+    view    = self.view
+    sel     = self.view.sel()
     max_col = max([view.rowcol(min(x.a, x.b))[1] for x in sel])
 
     for point in sel:
       selection_start = min(point.a, point.b)
-      rowcol = self.view.rowcol(selection_start)
-      self.view.insert(edit, selection_start, " " * (max_col - rowcol[1]))
+      rowcol          = self.view.rowcol(selection_start)
+      spaces          = " " * (max_col - rowcol[1])
+
+      self.view.insert(edit, selection_start, spaces)
+
+
+# TODO Reduce the number of spaces to the index
+class UseSelectionToAlignCommand(sublime_plugin.TextCommand):
+
+  def run(self, edit):
+    view         = self.view
+    block_to_arrange   = self.view.sel()
+    text_to_arrange = self.get_slurp_find_text()
+
+    if block_to_arrange is None: return
+    if text_to_arrange  is None: return
+
+    line_regions = view.lines(block_to_arrange[0])
+
+    max_index = max([
+      view.substr(view.line(cursor)).find(text_to_arrange)
+      for cursor in line_regions
+    ])
+
+    line_regions.reverse()
+
+    for cursor in line_regions:
+      line   = view.line(cursor)
+      substr = view.substr(line)
+      index  = substr.find(text_to_arrange)
+      start  = min(line.a, line.b) + index
+      rowcol = self.view.rowcol(start)
+      spaces = " " * (max_index - index)
+
+      self.view.insert(edit, start, spaces)
+
+
+  def get_slurp_find_text(self):
+    view = self.view
+    selected_region = self.view.sel()[0]
+
+    view.window().run_command("find_next")
+    view.window().run_command("find_prev")
+    sel = self.view.sel()
+
+    if len(sel) == 0: return None
+
+    text = view.substr(sel[0])
+    view.sel().clear()
+    view.sel().add(selected_region)
+
+    return text
+
+
--- readme.md
+++ readme.md
@@ -4,7 +4,28 @@
 
 Plugin for Sublime Text 2 and 3 for alignment in the column of cursors and text.
 
-* To align the cursors, press <kbd>⌘ cmd</kbd> + <kbd>\\</kbd> ( mac) or <kbd>^ Ctrl</kbd> + <kbd>\\</kbd> (linux, win);
-* To align the text, press <kbd>⌘ cmd</kbd> + <kbd>⌥ alt</kbd> + <kbd>\\</kbd> ( mac) or <kbd>^ Ctrl</kbd> + <kbd>Alt</kbd> + <kbd>\\</kbd> (linux, win).
+To align the cursors, press
+on ( mac)
+<kbd>⌘ cmd</kbd> + <kbd>\\</kbd>
+on (linux, win)
+<kbd>^ Ctrl</kbd> + <kbd>\\</kbd>.
+
+To align the text, press
+on ( mac)
+<kbd>⌘ cmd</kbd> + <kbd>⌥ alt</kbd> + <kbd>\\</kbd>
+on (linux, win)
+<kbd>^ Ctrl</kbd> + <kbd>Alt</kbd> + <kbd>\\</kbd>.
+
+To align in selection use
+on ( mac)
+- <kbd>⌘ cmd</kbd> + <kbd>E</kbd> to slurp find string
+- select region to align and press
+- <kbd>⌘ cmd</kbd> + <kbd>⌂ shift</kbd> + <kbd>\\</kbd> to align;
+
+on (linux, win)
+- <kbd>^ ctrl</kbd> + <kbd>E</kbd> to slurp find string
+- select region to align and press
+- <kbd>⌘ ctrl</kbd> + <kbd>⌂ shift</kbd> + <kbd>\\</kbd> to align.
+
 
 ![Demo](https://raw.githubusercontent.com/purelabio/sublime-vertical-align/master/demo.gif)
